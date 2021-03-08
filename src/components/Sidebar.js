7ed86ff13774aef5d5326d0b56fdaa7ca6f4dc79
royweb <xytank@gmail.com>
2021-03-08 15:34:48 -0500

    Add hr element and icons on the sidebar row component

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,6 +2,12 @@ import React from 'react';
 import HomeIcon from '@material-ui/icons/Home';
 import TrendingUpIcon from '@material-ui/icons/TrendingUp';
 import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
+import LocalLibraryIcon from '@material-ui/icons/LocalLibrary';
+import HistoryIcon from '@material-ui/icons/History';
+import PersonalVideoIcon from '@material-ui/icons/PersonalVideo';
+import WatchLaterIcon from '@material-ui/icons/WatchLater';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import MoreIcon from '@material-ui/icons/More';
 
 import './Sidebar.css';
 import SidebarRow from './SidebarRow';
@@ -12,6 +18,14 @@ function Sidebar() {
             <SidebarRow selected Icon={HomeIcon} title="Home" />
             <SidebarRow Icon={TrendingUpIcon} title="Trending" />
             <SidebarRow Icon={SubscriptionsIcon} title="Subscription" />
+            <hr />
+            <SidebarRow Icon={LocalLibraryIcon} title="Library" />
+            <SidebarRow Icon={HistoryIcon} title="History" />
+            <SidebarRow Icon={PersonalVideoIcon} title="Your Videos" />
+            <SidebarRow Icon={WatchLaterIcon} title="Watch Later" />
+            <SidebarRow Icon={FavoriteIcon} title="Favorite Videos" />
+            <SidebarRow Icon={MoreIcon} title="Show More" />
+            <hr />
         </div>
     );
 }
